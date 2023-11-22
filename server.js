429fa7c896bc71be490ee05980fc41555e392ea3
Ankit <ankitkumar19041@gmail.com>
2023-11-22 23:35:17 +0530

    added bsic proto srevice and implemented grpc server

---
+++ server.js
@@ -0,0 +1,40 @@
+const path = require("path");
+
+const grpc = require("@grpc/grpc-js");
+const protoLoader = require("@grpc/proto-loader");
+
+const packageDef = protoLoader.loadSync(
+  path.resolve(__dirname, "./chat.proto")
+);
+
+const grpcObject = grpc.loadPackageDefinition(packageDef);
+const chatPackage = grpcObject.chatpackage;
+
+function main() {
+  const server = getServer();
+  server.bindAsync(
+    "0.0.0.0:8000",
+    grpc.ServerCredentials.createInsecure(),
+    (err) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      console.log("Server running at http://locaclhost:8000");
+      server.start();
+    }
+  );
+}
+
+const getServer = () => {
+  const server = new grpc.Server();
+  server.addService(chatPackage.Chat.service, {
+    Yoho: (req, res) => {
+      console.log(req, res);
+    },
+  });
+
+  return server;
+};
+
+main();
